f3475e9ce5bf83d55d6f42077d5763affa793bd7
Samuel Lancaster <samuellancaster66@gmail.com>
2020-08-01 21:16:42 -0600

    pushing what changes I've made to the image folder

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,7 +1,14 @@
+import swellDwell from "../images/SwellDwell.png";
+import movieLover from "../images/MovieLover.png";
+import burgerLogger from "../images/Burger-log.png";
+import employeeTracker from "../images/Employee-tracker.png";
+import weatherTracker from "../images/Weather App.png";
+import formula from "../images/Formula-1-quiz1.png";
+
 export const Projects = [
         {
             id: 0,
-            image: "/images/SwellDwell.png",
+            image: swellDwell,
             title: "Swell Dwell",
             info: "A team of us created this website. Our main focus was the API calls. Our team really loved the idea of the website and if we had more time we would have added a lot more functionality.",
             site: "https://lancasting.github.io/Project-1/",
@@ -10,7 +17,7 @@ export const Projects = [
         },
         {
             id: 1,
-            image: "./images/MovieLover.png",
+            image: movieLover,
             title: "Movie Lover",
             info: "This is what we did for project 2 of our Bootcamp. My team really excelled in communicating what we were working on and if anyone needed help we all jumped in to lend a hand. The main focus was the CRUD operations of our SQL database.",
             site: "https://projectdash.herokuapp.com/",
@@ -19,7 +26,7 @@ export const Projects = [
         },
         {
             id: 2,
-            image: "./images/Burger-log.png",
+            image: burgerLogger,
             title: "Burger Log",
             info: "This is basic burger logger I created. Emphasizing using some of the CRUD operations.",
             site: "https://stormy-hollows-97400.herokuapp.com/",
@@ -28,7 +35,7 @@ export const Projects = [
         },
         {
             id: 3,
-            image: "./images/Employee-tracker.png",
+            image: employeeTracker,
             title: "Employee Tracker",
             info: "This is a simple employee tracker that you run through node. Basic SQL CRUD operations managing a database of employees",
             site: "",
@@ -37,7 +44,7 @@ export const Projects = [
         },
         {
             id: 4,
-            image: "./images/Weather App.png",
+            image: weatherTracker,
             title: "Weather App",
             info: "I created a basic weather app. Mainly focused on the interworkings of the API calls",
             site: "https://lancasting.github.io/Weather-App/",
@@ -46,7 +53,7 @@ export const Projects = [
         },
         {
             id: 5,
-            image: "./images/Formula 1 quiz.png",
+            image: formula,
             title: "Quiz",
             info: "A Formula 1 quiz I created. Javascript was the main focus of this.",
             site: "https://lancasting.github.io/F1-Quiz/",
