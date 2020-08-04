e9d5033df362ab0cad7008aa361134687f049aef
Samuel Lancaster <samuellancaster66@gmail.com>
2020-08-03 21:18:19 -0600

    Making small changes to styling

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./style.css";
 
 function Contact() {
     return (
@@ -9,15 +8,15 @@ function Contact() {
                     <label htmlFor="name">Phone Number: (307) 248-1134</label>
                 </div>
                 <div className="form-group">
-                    <label htmlFor="email">Email: </label> <a href="mailto:samuellancaster66@gmail.com">samuellancaster66@gmail.com</a>
+                    <label htmlFor="email">Email: </label> <a href="mailto:samuellancaster66@gmail.com"> samuellancaster66@gmail.com</a>
                 </div>
                 <div className="form-group">
                     <label htmlFor="message">Github: </label>
-                    <a href="https://github.com/Lancasting" className="card-link" target="_blank" rel="noopener noreferrer">Lancasting</a>
+                    <a href="https://github.com/Lancasting" className="card-link" target="_blank" rel="noopener noreferrer"> Lancasting</a>
                 </div>
                 <div className="form-group">
                     <label htmlFor="linkedin">Linkedin: </label>
-                    <a href="https://www.linkedin.com/in/samuellancaster" className="card-link" target="_blank" rel="noopener noreferrer">Samuel Lancaster</a>
+                    <a href="https://www.linkedin.com/in/samuellancaster" className="card-link" target="_blank" rel="noopener noreferrer"> Samuel Lancaster</a>
                 </div>
             </form>
         </section>
