c21c4611c13dfb16ab7b2654c1d1dd9739e11fbb
Samuel Lancaster <samuellancaster66@gmail.com>
2020-08-04 22:14:16 -0600

    Adding switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -16,12 +16,14 @@ function App() {
       <div>
       <Wrapper>
         <Navbar />
+        <Switch>
           <Route exact path="/" component={Home} />
           {/* <Route exact path="/home" component={Home}/> */}
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
+          </Switch>
           <Footer />
-        </Wrapper>
+      </Wrapper>
       </div>
     </Router>
     // </div>
