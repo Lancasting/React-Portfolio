782945719408b133876eaee362f39389999a3477
Samuel Lancaster <samuellancaster66@gmail.com>
2020-08-04 22:01:05 -0600

    Updates

---
+++ precache-manifest.531484562877a8f95479ced6810aa3d7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7394785ac028cd7fa367fcb3cdfe4240",
+    "url": "/React-Porfolio/index.html"
+  },
+  {
+    "revision": "25699515a995bf9e1166",
+    "url": "/React-Porfolio/static/css/main.7bc19368.chunk.css"
+  },
+  {
+    "revision": "d379768fd117b84fce7c",
+    "url": "/React-Porfolio/static/js/2.d5714cb1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Porfolio/static/js/2.d5714cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25699515a995bf9e1166",
+    "url": "/React-Porfolio/static/js/main.f2fc86bc.chunk.js"
+  },
+  {
+    "revision": "7305f15ec684b47e4e2e",
+    "url": "/React-Porfolio/static/js/runtime-main.198f7da9.js"
+  },
+  {
+    "revision": "a98f68e922a851b7e6b9a1ec71ce7602",
+    "url": "/React-Porfolio/static/media/Burger-log.a98f68e9.png"
+  },
+  {
+    "revision": "c8fab9dc46396841ec3626be0361e094",
+    "url": "/React-Porfolio/static/media/Employee-tracker.c8fab9dc.png"
+  },
+  {
+    "revision": "71b9274de8af366382282e624a008d78",
+    "url": "/React-Porfolio/static/media/Formula-1-quiz1.71b9274d.png"
+  },
+  {
+    "revision": "1398eb699bbc41e53df85b1ef5f2f8d5",
+    "url": "/React-Porfolio/static/media/MovieLover.1398eb69.png"
+  },
+  {
+    "revision": "a7dd3229373e6807b8db8de01391685f",
+    "url": "/React-Porfolio/static/media/SwellDwell.a7dd3229.png"
+  },
+  {
+    "revision": "c9793e46abbf9df7bc43b5ab8e06bb78",
+    "url": "/React-Porfolio/static/media/Weather App.c9793e46.png"
+  },
+  {
+    "revision": "d59bd6be03822c44ab8b37c26781ee84",
+    "url": "/React-Porfolio/static/media/profile.d59bd6be.jpg"
+  },
+  {
+    "revision": "fbd72cb4f2e92bf35489aff85e7aba68",
+    "url": "/React-Porfolio/static/media/snowbird.fbd72cb4.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.c0592a91caeab2c2cda7e1bccd52527e.js"
+  "/React-Porfolio/precache-manifest.531484562877a8f95479ced6810aa3d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Porfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
